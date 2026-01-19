6d24891f6697a9491277b0d48bcdac0e39d90619
Mik0-T3ch <chrispino2109@gmail.com>
2026-01-19 02:38:21 -0600

    creacion de la carpeta uploads y edicion de server.py

--- server/server.py
+++ server/server.py
@@ -2,14 +2,17 @@ from flask import Flask
 import os
 from handlers.claves import config as cg
 from server.routes import routes_bp
-from server.auth import auth_bp   # ← importante también
+from server.auth import auth_bp 
+UPLOAD_FOLDER = "uploads"
 
 app = Flask(__name__)
 
-app.secret_key = cg.KEY_SECRET
+app.secret_key = cg.flask_secret
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(routes_bp)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET'])
 def home():
